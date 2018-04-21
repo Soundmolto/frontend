94a4974754000d6717e0dcdffc0ab3cc8da78779
Jake Jarrett <jakeryanjarrett@gmail.com>
2018-04-21 17:16:45 +1000

    Start of adding global shortcuts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+import { Component } from 'preact';
 import { Provider, connect } from 'preact-redux';
+import { HotKeys } from 'react-hotkeys';
 import App from './components/App';
 import store from './store';
 import './style';
 
+class HotKeysHOC extends Component {
+	map = {
+		'show:settings': 'ctrl+,'
+	};
+
+	handlers = {
+		'show:settings': e => console.log(e)
+	};
+
+	render ({ children }) {
+		return (
+			<HotKeys handlers={this.handlers} keyMap={this.map}>
+				{children}
+			</HotKeys>
+		);
+	}
+};
 
 const Main = () => (
 	<Provider store={store}>
-		<App />
+		<HotKeysHOC>
+			<App />
+		</HotKeysHOC>
 	</Provider>
 );
 
