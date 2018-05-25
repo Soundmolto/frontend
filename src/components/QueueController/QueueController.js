db9089c89dfbf743adee1c955b35a845974ab7cb
Jake <jakeryanjarrett@gmail.com>
2018-05-25 19:10:49 +1000

    Work on implementing queue system.

---
+++ src/components/QueueController/QueueController.js
@@ -0,0 +1,60 @@
+let position = 0;
+let tracks = [];
+
+export class QueueController {
+	
+	get tracks () {
+		return tracks.concat([]);
+	}
+
+	set tracks (t = []) {
+		if (Array.isArray(t) === false) {
+			throw new Error('Tracks must be an array.');
+		}
+		tracks = t;
+	}
+
+	get current () {
+		return tracks[position];
+	}
+
+	first () {
+		return tracks[0];
+	}
+
+	last () {
+		return tracks[tracks.length - 1];
+	}
+
+	next () {
+		position += 1;
+		return tracks[position];
+	}
+
+	previous () {
+		position -= 1;
+		return tracks[position];
+	}
+
+	shuffle () {
+		let currentIndex = tracks.length;
+		let temporaryValue;
+		let randomIndex;
+  
+		// While there remain elements to shuffle...
+		while (0 !== currentIndex) {
+	
+			// Pick a remaining element...
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+		
+			// And swap it with the current element.
+			temporaryValue = tracks[currentIndex];
+			tracks[currentIndex] = tracks[randomIndex];
+			tracks[randomIndex] = temporaryValue;
+		}
+	
+		tracks = array.concat([]);
+	}
+
+}
---
+++ src/components/QueueController/index.js
@@ -0,0 +1,1 @@
+export { QueueController } from './QueueController';
