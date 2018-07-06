8ea7c2ae6c88fb924fc34a2bf244d9c39e72cef6
Jake <jakeryanjarrett@gmail.com>
2018-07-07 09:25:26 +1000

    Add validation for password length

--- src/routes/register/index.js
+++ src/routes/register/index.js
@@ -15,13 +15,37 @@ import Helmet from 'preact-helmet';
 import { APP } from '../../enums/app';
 
 // State outside of the component.
-let state = {};
+let _state = {};
 
 /**
  * The login page / component
  */
 @connect(({ auth }) => auth)
 export default class Login extends Component {
+
+	state = {
+		error: false,
+		errorMessage: ''
+	};
+
+	validate () {
+		let valid = true;
+
+		if (_state.password == null) {
+			this.setState({ error: true, errorMessage: 'Password must set.' });
+			valid = false;
+		}
+		if (_state.password.length < 8) {
+			this.setState({ error: true, errorMessage: 'Password must be longer than 8 characters.' });
+			valid = false;
+		} 
+		
+		if (valid === true) {
+			this.setState({ error: false, errorMessage: '' });
+		}
+
+		return valid;
+	}
 
 	/**
 	 * When the user submits the login form.
@@ -32,13 +56,15 @@ export default class Login extends Component {
 	 * 
 	 * @param {Event} e - The submit event.
 	 */
-	async onLogin (e) {
+	async onRegister (e) {
 		e.preventDefault();
 		const { dispatch } = this.props;
-		/** This will update the UI to indicate we're logging in */
-		dispatch(begin_register());
-		/** This will update our state to indicate we've either logged in or failed login */
-		register(state, dispatch, _ => state = {});
+		if (this.validate()) {
+			/** This will update the UI to indicate we're logging in */
+			dispatch(begin_register());
+			/** This will update our state to indicate we've either logged in or failed login */
+			register(_state, dispatch, _ => state = {});
+		}
 	}
 
 	/**
@@ -47,7 +73,7 @@ export default class Login extends Component {
 	 * @param {Event|Object} event - The change event
 	 */
 	onEmailChange (event) {
-		state = {...state, email: event.currentTarget.value || ""};
+		_state = {..._state, email: event.currentTarget.value || ""};
 	}
 
 	/**
@@ -56,10 +82,14 @@ export default class Login extends Component {
 	 * @param {Event|Object} event - The change event
 	 */
 	onPasswordChange (event) {
-		state = {...state, password: event.currentTarget.value || ""};
+		_state = {..._state, password: event.currentTarget.value || ""};
 	}
 
-	render ({ loading, logged_in, error, errorMessage }) {
+	shouldComponentUpdate () {
+		return true;
+	}
+
+	render ({ loading, logged_in, error, errorMessage }, state) {
 		if (logged_in === true) route("/", true);
 
 		return (
@@ -70,15 +100,16 @@ export default class Login extends Component {
 				</div>
 				<div class={style.home}>
 					<Card className={style.card}>
-						<form class={style.cardBody} onSubmit={this.onLogin.bind(this)} key={"registration-form"}>
+						<form class={style.cardBody} onSubmit={this.onRegister.bind(this)} key={"registration-form"}>
 							<TextField label="Enter your email address" type="email" autofocus onChange={this.onEmailChange.bind(this)} key="registration-email" />
 							<TextField type="password" label="Enter a password" onChange={this.onPasswordChange.bind(this)} key="registration-password" />
 							<div className={style.buttonContainer}>
-								<Button raised onClick={this.onLogin.bind(this)} type="submit">
+								<Button raised onClick={this.onRegister.bind(this)} type="submit">
 									{!logged_in && !loading && "Register"}
 									{!logged_in && loading && <LinearProgress reversed={true} indeterminate={true} />}
 								</Button>
 								{error && errorMessage != null && <div className="error-message">{errorMessage}</div>}
+								{state.error && state.errorMessage != null && <div className="error-message">{state.errorMessage}</div>}
 							</div>
 						</form>
 					</Card>
